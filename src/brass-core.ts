e7c22fd1bcd225cb07fb38036bbc3eb110ed2ebe
wangyidong <280532379@qq.com>
2022-04-19 17:37:17 +0800

    1.add shareReplay; 2. add brassData$

--- src/brass-core.ts
+++ src/brass-core.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, distinctUntilChanged, scan, map, mergeWith, Observable, ReplaySubject,debounceTime, skip } from 'rxjs'
+import { BehaviorSubject, distinctUntilChanged, scan, map, mergeWith, Observable, ReplaySubject,debounceTime, combineLatest, shareReplay } from 'rxjs'
 import { produce, current} from "immer"
 
 export interface Store {
@@ -9,28 +9,45 @@ export interface Store {
 export interface Streams {
     [key: string]: Observable<any>
 }
-const currying2 = function (fn: any) {
-    return  function() {
-        return fn
-    }
-};
+export interface DraftFn {
+  (draft: any): any
+}
+
+// const currying2 = function (fn: any) {
+//     return  function() {
+//         return fn
+//     }
+// };
 export const createBrass = (initial_state: any, reducers: any, getters: any) => { 			      
     let streams: Streams = {}, actions:any = {}, state$
+    // 处理 setState 方法
+    const initSetState = () => {
+      const subject$ = new ReplaySubject(1) 
+      const streamAction: any = (args:[DraftFn]) => {
+        return produce(args[0])
+      }
+      streams['setState$'] = subject$.pipe(map(streamAction))
+      return (...args: any) => subject$.next(args)
+    }
+    const setState = initSetState()
+    // 处理 reducers
     Object.keys(reducers).forEach((action: any) =>{
         const subject$ = new ReplaySubject(1) 	
         const streamAction = (args: any) => produce(reducers[action](...args))
-        streams[`${action}$`] = subject$.pipe(map(streamAction))	
         actions[action] = (...args: any) => subject$.next(args)
+        streams[`${action}$`] = subject$.pipe(map(streamAction))
     })
     state$ = new BehaviorSubject(initial_state).pipe(      
         mergeWith(...Object.values(streams)),					          
-        scan((state: any, reducer: any) => reducer(state)),
-        distinctUntilChanged((pre: any, cur: any) => { 
+        scan((state, reducer) => reducer(state)),
+        distinctUntilChanged((pre, cur) => { 
             return JSON.stringify(pre) === JSON.stringify(cur)}),
+        // share({replay:1})
+        shareReplay(1)
     )	
-
+    // 处理 getters
     const getters$ = state$.pipe(
-        map((state: any) =>
+        map((state) =>
             Object.assign(
                 {},
                 ...Object.keys(getters).map(key => {
@@ -38,15 +55,16 @@ export const createBrass = (initial_state: any, reducers: any, getters: any) => 
                 })
             )
         ),
-        distinctUntilChanged((pre: any, cur: any) => JSON.stringify(pre) === JSON.stringify(cur)),
-    ) 
-    return {state$, actions, getters$} 										               
+        distinctUntilChanged((pre, cur) => JSON.stringify(pre) === JSON.stringify(cur)),
+    )
+    const brassData$ = combineLatest({state: state$, getters: getters$})
+    return {state$, actions, getters$, setState, brassData$} 										               
 } 
 
 export const brassLogger = (prefix: string, observable: Observable<any>) => {
     return observable.pipe(
         debounceTime(1000),
-        scan((prevState: any, nextState: any) => {
+        scan((prevState, nextState) => {
             console.groupCollapsed(`${prefix}`)
         
             console.log(`%c prev state:`, `color: #999999; font-weight: bold`, prevState)
